ccd1b55489f484b447567a22dd9bde0360bf89ea
jam3sl1ng <jamesling596@gmail.com>
2026-05-15 17:05:50 +0100

    V0.7

--- code/player.py
+++ code/player.py
@@ -1,15 +1,21 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((48, 56))
         self.image.fill('red')
+
+        # Rects
         self.rect = self.image.get_frect(topleft = pos)
+        self.old_rect = self.rect.copy()
 
         # Movement 
         self.direction = vector()
         self.speed = 500
+
+        # Collision
+        self.collision_sprites = collision_sprites
     
     def input(self):
         keys = pygame.key.get_pressed()
@@ -23,8 +29,26 @@ class Player(pygame.sprite.Sprite):
         self.direction = input_vector.normalize() if input_vector else input_vector
 
     def move(self, dt):
-        self.rect.topleft += self.direction * self.speed * dt
+        self.rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+    
+    def collision(self, axis):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.rect):
+                if axis == 'horizontal':
+                    # Left
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+
+                    # Right
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                else: # Vertical
+                    pass 
 
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(dt)
